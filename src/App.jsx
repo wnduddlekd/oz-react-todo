42e5ed54db9cb2759808e01697c3e2ef5705c7f9
wndud <ju03209@naver.com>
2025-05-22 13:41:08 +0900

    feat: Todo 컴포넌트 수정 버튼 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -52,24 +52,36 @@ function TodoList({ todoList, setTodoList }) {
 
 function Todo({ todo, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
+  const [showInput, setShowInput] = useState(false);
+
+  const handleToggle = () => {
+    if (showInput) {
+      if (inputValue) {
+        setTodoList((prev) =>
+          prev.map((el) =>
+            el.id === todo.id ? { ...el, content: inputValue } : el
+          )
+        );
+        setInputValue("");
+        setShowInput(false);
+      } else {
+        setShowInput(false);
+      }
+    } else {
+      setShowInput(true);
+    }
+  };
   return (
     <li>
       {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
+      {showInput && (
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="수정할 Todo List 입력"
+        />
+      )}
+      <button onClick={handleToggle}>수정</button>
       <button
         onClick={() => {
           setTodoList((prev) => {
